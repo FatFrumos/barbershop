07f979de3b2a99a613d5d2abc0c85eb88ce40124
FatFrumos <alex13699@gmail.com>
2016-07-05 01:34:14 +0300

    Открытие и закрытие модальных окон

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+var login = document.querySelector(".login");
+login.addEventListener("click", function (Event) {
+    "use strict";
+    event.preventDefault();
+    var popup = document.querySelector(".modal-login");
+    popup.classList.add("modal-show");
+});
 
+var close = document.querySelector(".modal-close");
+close.addEventListener("click", function (Event) {
+    "use strict";
+    event.preventDefault();
+    var popup = document.querySelector(".modal-login");
+    popup.classList.remove("modal-show");
+});
+
+
+var map = document.querySelector(".find");
+map.addEventListener("click", function (Event) {
+    "use strict";
+    Event.preventDefault();
+    var popup = document.querySelector(".modal-map");
+    popup.classList.add("modal-show");
+});
+
+var close = document.querySelector(".modal-map .modal-close");
+close.addEventListener("click", function (Event) {
+    "use strict";
+    event.preventDefault();
+    var popup = document.querySelector(".modal-map");
+    popup.classList.remove("modal-show");
+});
